cc65c7d0015ee849bf40a7f1532e3e70125a6867
Olutosin Bamigboye <52960522+tob103@users.noreply.github.com>
2026-02-20 13:18:33 -0600

    Extended Docker custom Action files

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,4 +1,38 @@
+import os
+import requests
+import time
+
+def ping_url(url, delay, max_trials):
+    trials = 0
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable")
+                return True
+        except requests.exceptions.ConnectionError as e:
+            print(f"Error pinging {url}. Retrying in {delay} seconds.: {e}")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL format:{url}. Retrying in {delay} seconds.: {e}")
+            time.sleep(delay)
+            trials += 1
+    return False
+
+def run():
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    website_reachable = ping_url(website_url, delay, max_trials)
+
+    if website_reachable:
+        print(f"Website {website_url} is reachable")
+    else:
+        print(f"Website {website_url} is not reachable")
+
 if __name__ == "__main__":
-    print("Hello World")
+    run()
 
-
+#Normally you should have a way of validating the inputs and parameters
